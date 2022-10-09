936de5716748900c85b653e6257fe00a0896e7fe
Abhishek <abhiajoy23@gmail.com>
2022-10-09 17:31:39 +0530

    Login card/service done, Navbar in progress

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -1,20 +1,29 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import {HttpClientModule} from '@angular/common/http';
+import { FormsModule } from '@angular/forms';
+
+import {MatInputModule} from '@angular/material/input';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { NavComponent } from './nav/nav.component';
+import { LoginComponent } from './login/login.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavComponent,
+    LoginComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     HttpClientModule,
-    BrowserAnimationsModule
+    FormsModule,
+    BrowserAnimationsModule,
+    MatInputModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ client/src/app/login/login.component.ts
@@ -0,0 +1,29 @@
+import { Component, OnInit } from '@angular/core';
+import { AccountService } from '../services/account.service';
+
+@Component({
+  selector: 'app-login',
+  templateUrl: './login.component.html',
+  styleUrls: ['./login.component.css']
+})
+export class LoginComponent implements OnInit {
+
+  model:any = {};
+  loggedIn:boolean;
+  constructor(private accountService: AccountService) { }
+
+  ngOnInit(): void {
+  }
+
+  login(){
+    this.accountService.login(this.model).subscribe({
+      next: response => {console.log(response);this.loggedIn = true},
+      error: error => console.log(error)
+    });
+  }
+
+  logout(){
+    this.loggedIn = false;
+  }
+
+}
